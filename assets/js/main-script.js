7670a4701a28fb6256eb08fb19e4e65cacf2763c
Mahmudul607 <mahmudul.cse.eng@gmail.com>
2022-11-26 10:34:06 +0600

    testimonial

--- assets/js/main-script.js
+++ assets/js/main-script.js
@@ -30,5 +30,22 @@ $(document).ready(function(){
 // ==========For calculator==========
 AOS.init();
 
-//  =============Accordion============//
+//  =============Accordion owl============//
 
+$('.owl-carousel').owlCarousel({
+   loop:true,
+   margin:10,
+   nav:true,
+   dots:false,
+   responsive:{
+       0:{
+           items:1
+       },
+       600:{
+           items:1
+       },
+       1000:{
+           items:1
+       }
+   }
+})
